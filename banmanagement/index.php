d3a0271e3764b706fbaafb9cb7de7ddd41073fd1
confuser <jamesmortemore@gmail.com>
2012-12-13 19:30:03 +0000

    Added APC support. Checks on page load, if not enabled, will revert to normal file based cache.

--- banmanagement/index.php
+++ banmanagement/index.php
@@ -218,64 +218,65 @@ function cache($query, $time, $folder = '', $server = array(), $name = '') {
 	global $settings;
 	$md5 = md5($query);
 	if($folder == '' && empty($name))
-		$file = 'cache/'.$md5.'.php';
+		$file = $md5;
 	else if($folder != '' && empty($name))
-		$file = 'cache/'.$folder.'/'.$md5.'.php';
+		$file = $folder.'/'.$md5;
 	else if($folder != '' && !empty($name))
-		$file = 'cache/'.$folder.'/'.$name.'.php';
+		$file = $folder.'/'.$name;
 	else if($folder == '' && !empty($name))
-		$file = 'cache/'.$name.'.php';
-	$file = IN_PATH.$file;
-	if($folder != '' && !is_dir(IN_PATH.'cache/'.$folder))
-		mkdir(IN_PATH.'cache/'.$folder, 0777, true);
-	if(file_exists($file)) {
-		if(time() - filemtime($file) > $time) {
-			// Needs recache
-			if(!empty($server)) {
-				if(isset($settings['last_connection'])) {
-					$diff = array_diff($settings['last_connection'], $server);
-					if(!empty($diff))
-						connect($server);
-				} else
-					connect($server);
-			}
-			$sql = mysql_query($query);
-			$data = array();
-			if(mysql_num_rows($sql) > 0) {
-				while($fetch = mysql_fetch_array($sql)) // Loop through the data
-					array_push($data, $fetch);
-			}
-			// Check if its only one row
-			if(count($data) == 1)
-				$data = $data[0];
-			// Now save it
-			file_put_contents($file, "<?php die(); ?>\n".serialize($data)); // Create the file
-			return $data; // Return the fresh data
-		} else {
-			// Serve the cache
-			return unserialize(file_get_contents($file, NULL, NULL, 16));
+		$file = $name;
+	
+	if($settings['apc_enabled']) {
+		if(apc_exists($file))
+			return apc_fetch($file);
+		else {
+			return createCache($query, $server, $file, $time);
 		}
 	} else {
-		// Cache needs creating
-		if(!empty($server)) {
-			if(isset($settings['last_connection'])) {
-				$diff = array_diff($settings['last_connection'], $server);
-				if(!empty($diff))
-					connect($server);
-			} else
-				connect($server);
+		$file = IN_PATH.'cache/'.$file.'.php';
+		if($folder != '' && !is_dir(IN_PATH.'cache/'.$folder))
+			mkdir(IN_PATH.'cache/'.$folder, 0777, true);
+		if(file_exists($file)) {
+			if(time() - filemtime($file) > $time) {
+				// Needs recache
+				return createCache($query, $server, $file); // Return the fresh data
+			} else {
+				// Serve the cache
+				return unserialize(file_get_contents($file, NULL, NULL, 16));
+			}
+		} else {
+			// Cache needs creating
+			return createCache($query, $server, $file); // Return the fresh data
 		}
-		$sql = mysql_query($query);
-		$data = array();
+	}
+}
+
+function createCache($query, $server, $file, $time = 0) {
+	global $settings;
+
+	if(!empty($server)) {
+		if(isset($settings['last_connection'])) {
+			$diff = array_diff($settings['last_connection'], $server);
+			if(!empty($diff))
+				connect($server);
+		} else
+			connect($server);
+	}
+	$sql = mysql_query($query);
+	$data = array();
+	if(mysql_num_rows($sql) > 0) {
 		while($fetch = mysql_fetch_array($sql)) // Loop through the data
 			array_push($data, $fetch);
-		// Check if its only one row
-		if(count($data) == 1)
-			$data = $data[0];
-		// Now save it
-		file_put_contents($file, "<?php die(); ?>\n".serialize($data)); // Create the file
-		return $data; // Return the fresh data
 	}
+	// Check if its only one row
+	if(count($data) == 1)
+		$data = $data[0];
+	// Now save it
+	if(!$settings['apc_enabled'])
+		file_put_contents($file, "<?php die(); ?>\n".serialize($data)); // Create the file
+	else
+		apc_store($file, $data, $time);
+	return $data; // Return the fresh data
 }
 
 function rglob($pattern='*', $flags = 0, $path='') {
@@ -336,6 +337,11 @@ else
 	errors('You must rename settingsRename.php to settings.php');
 
 $settings['servers'] = unserialize($settings['servers']);
+
+// Check if APC is enabled to use that instead of file cache
+$settings['apc_enabled'] = false;
+if(extension_loaded('apc') && ini_get('apc.enabled'))
+	$settings['apc_enabled'] = true;
 
 if(!isset($_GET['ajax']) || (isset($_GET['ajax']) && !$_GET['ajax']))
 	include('header.php');
