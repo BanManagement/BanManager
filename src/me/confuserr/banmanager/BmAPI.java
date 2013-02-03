81afb3e5cb3e963a66913d1ebcba63798bdbd9a1
confuser <jamesmortemore@gmail.com>
2013-02-03 02:14:42 +0000

    Added basic API

---
+++ src/me/confuserr/banmanager/BmAPI.java
@@ -0,0 +1,63 @@
+package me.confuserr.banmanager;
+
+class BmAPI {
+
+	private BanManager plugin = BanManager.getPlugin();
+
+	// Bans
+	public void ban(String player, String banned_by, String reason) {
+		plugin.dbLogger.logBan(player, banned_by, reason);
+
+		if (plugin.bukkitBan) {
+			plugin.getServer().getOfflinePlayer(player).setBanned(true);
+		}
+	}
+
+	public void tempban(String player, String banned_by, String reason, long expires) {
+		plugin.dbLogger.logTempBan(player, banned_by, reason, expires);
+
+		if (plugin.bukkitBan) {
+			plugin.getServer().getOfflinePlayer(player).setBanned(true);
+		}
+	}
+
+	public void unban(String player, String by) {
+		plugin.dbLogger.banRemove(player, by);
+
+		if (plugin.bukkitBan) {
+			plugin.getServer().getOfflinePlayer(player).setBanned(false);
+		}
+	}
+
+	public boolean isBanned(String player) {
+		return !plugin.dbLogger.isBanned(player).isEmpty();
+	}
+
+	// public BanData getCurrentBan(String player);
+
+	// Mutes
+	public void mute(String player, String muted_by, String reason) {
+		if (!plugin.dbLogger.isMuted(player)) {
+			plugin.addMute(player, reason, muted_by, (long) 0);
+			plugin.dbLogger.logMute(player, muted_by, reason);
+		}
+	}
+
+	public void tempmute(String player, String reason, String muted_by, long expires) {
+		if (!plugin.dbLogger.isMuted(player)) {
+			plugin.addMute(player, reason, muted_by, expires);
+			plugin.dbLogger.logMute(player, muted_by, reason);
+		}
+	}
+
+	public void unmute(String player, String by) {
+		if(plugin.dbLogger.isMuted(player))
+			plugin.removeMute(player, player);
+	}
+
+	public boolean isMuted(String player) {
+		return plugin.dbLogger.isMuted(player);
+	}
+
+	// public MuteData getCurrentMute(String player);
+}
