8d79521691192867ae0da6cc0e3ccbddec3239da
confuser <jamesmortemore@gmail.com>
2013-02-21 18:41:42 +0000

    Threaded unbanip command and added check for bukkitBan

--- src/me/confuserr/banmanager/Commands/UnBanIpCommand.java
+++ src/me/confuserr/banmanager/Commands/UnBanIpCommand.java
@@ -2,7 +2,6 @@ package me.confuserr.banmanager.Commands;
 
 import me.confuserr.banmanager.BanManager;
 
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,50 +14,59 @@ public class UnBanIpCommand implements CommandExecutor {
 	public UnBanIpCommand(BanManager instance) {
 		plugin = instance;
 	}
-	
+
 	@Override
-	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String args[]) {		
-		if(args.length < 1)
+	public boolean onCommand(final CommandSender sender, Command command, String commandLabel, String args[]) {
+		if (args.length < 1)
 			return false;
-		
+
 		Player player = null;
 		String playerName = plugin.banMessages.get("consoleName");
-		
-		if(sender instanceof Player) {
+
+		if (sender instanceof Player) {
 			player = (Player) sender;
 			playerName = player.getName();
-			if(!player.hasPermission("bm.unbanip")) {
+			if (!player.hasPermission("bm.unbanip")) {
 				plugin.sendMessage(player, plugin.banMessages.get("commandPermissionError"));
 				return true;
 			}
 		}
-		
-		if(BanManager.ValidateIPAddress(args[0])) {
+
+		if (BanManager.ValidateIPAddress(args[0])) {
 			// Its an IP
 			String ip = args[0];
-			if(plugin.dbLogger.ipBanned(ip)) {
-				plugin.getServer().unbanIP(ip);
+			if (plugin.bannedIps.contains(ip)) {
+				if (plugin.bukkitBan)
+					plugin.getServer().unbanIP(ip);
+
 				plugin.dbLogger.ipRemove(ip, playerName);
 				plugin.sendMessage(sender, plugin.banMessages.get("ipUnbanned").replace("[ip]", ip));
 			} else {
 				plugin.sendMessage(sender, plugin.banMessages.get("ipNotBannedError").replace("[ip]", ip));
 			}
-			
+
 		} else {
 			// Assume its a player!
-			OfflinePlayer offlinePlayer = plugin.getServer().getOfflinePlayer(args[0]);
+			final String offlineName = plugin.getServer().getOfflinePlayer(args[0]).getName();
+			final String byName = playerName;
 			
-			String ip = plugin.dbLogger.getIP(offlinePlayer.getName());
+			plugin.getServer().getScheduler().scheduleAsyncDelayedTask(plugin, new Runnable() {
 
-			if(ip.isEmpty())
-				plugin.sendMessage(sender, plugin.banMessages.get("ipPlayerOfflineError").replace("[name]", offlinePlayer.getName()));
-			else {
-				// Ok, we have their IP, lets ban it
-				plugin.getServer().unbanIP(ip);
-				plugin.dbLogger.ipRemove(ip, playerName);
-				plugin.sendMessage(sender, plugin.banMessages.get("ipUnbanned").replace("[ip]", ip));
-			}
-			//plugin.sendMessage(sender, plugin.banMessages.get("invalidIp"));
+				public void run() {
+					String ip = plugin.dbLogger.getIP(offlineName);
+
+					if (ip.isEmpty())
+						plugin.sendMessage(sender, plugin.banMessages.get("ipPlayerOfflineError").replace("[name]", offlineName));
+					else {
+						// Ok, we have their IP, lets ban it
+						if(plugin.bukkitBan)
+							plugin.getServer().unbanIP(ip);
+						
+						plugin.dbLogger.ipRemove(ip, byName);
+						plugin.sendMessage(sender, plugin.banMessages.get("ipUnbanned").replace("[ip]", ip));
+					}
+				}
+			});
 		}
 
 		return true;
