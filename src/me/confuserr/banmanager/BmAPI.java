8dc3a372104917099d49bc7f5d3645572e529b2c
confuser <jamesmortemore@gmail.com>
2013-04-05 21:38:40 +0100

    Fixed inaccessibility to BmAPI

--- src/me/confuserr/banmanager/BmAPI.java
+++ src/me/confuserr/banmanager/BmAPI.java
@@ -3,12 +3,12 @@ package me.confuserr.banmanager;
 import me.confuserr.banmanager.data.BanData;
 import me.confuserr.banmanager.data.MuteData;
 
-class BmAPI {
+public class BmAPI {
 
-	private BanManager plugin = BanManager.getPlugin();
+	private static BanManager plugin = BanManager.getPlugin();
 
 	// Bans
-	public void ban(String player, String banned_by, String reason) {
+	public static void ban(String player, String banned_by, String reason) {
 		plugin.dbLogger.logBan(player, banned_by, reason);
 
 		if (plugin.bukkitBan) {
@@ -16,7 +16,7 @@ class BmAPI {
 		}
 	}
 
-	public void tempban(String player, String banned_by, String reason, long expires) {
+	public static void tempban(String player, String banned_by, String reason, long expires) {
 		plugin.dbLogger.logTempBan(player, banned_by, reason, expires);
 
 		if (plugin.bukkitBan) {
@@ -24,7 +24,7 @@ class BmAPI {
 		}
 	}
 
-	public void unban(String player, String by) {
+	public static void unban(String player, String by) {
 		plugin.dbLogger.banRemove(player, by);
 
 		if (plugin.bukkitBan) {
@@ -32,39 +32,39 @@ class BmAPI {
 		}
 	}
 
-	public boolean isBanned(String player) {
+	public static boolean isBanned(String player) {
 		return !plugin.dbLogger.isBanned(player).isEmpty();
 	}
 
-	public BanData getCurrentBan(String player) {
+	public static BanData getCurrentBan(String player) {
 		return plugin.dbLogger.getCurrentBan(player);
 	}
 
 	// Mutes
-	public void mute(String player, String muted_by, String reason) {
+	public static void mute(String player, String muted_by, String reason) {
 		if (!plugin.dbLogger.isMuted(player)) {
 			plugin.addMute(player, reason, muted_by, (long) 0);
 			plugin.dbLogger.logMute(player, muted_by, reason);
 		}
 	}
 
-	public void tempmute(String player, String reason, String muted_by, long expires) {
+	public static void tempmute(String player, String reason, String muted_by, long expires) {
 		if (!plugin.dbLogger.isMuted(player)) {
 			plugin.addMute(player, reason, muted_by, expires);
 			plugin.dbLogger.logMute(player, muted_by, reason);
 		}
 	}
 
-	public void unmute(String player, String by) {
+	public static void unmute(String player, String by) {
 		if(plugin.dbLogger.isMuted(player))
 			plugin.removeMute(player, player);
 	}
 
-	public boolean isMuted(String player) {
+	public static boolean isMuted(String player) {
 		return plugin.dbLogger.isMuted(player);
 	}
 
-	public MuteData getCurrentMute(String player) {
+	public static MuteData getCurrentMute(String player) {
 		return plugin.dbLogger.getCurrentMute(player);
 	}
 }
