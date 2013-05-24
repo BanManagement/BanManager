4538f41951d1452da97ff1037ed589515570f108
confuser <jamesmortemore@gmail.com>
2013-05-24 16:27:58 +0100

    Searching sort persistant on page reload

--- banmanagement/js/core.js
+++ banmanagement/js/core.js
@@ -64,7 +64,7 @@ $(function() {
 
 		// widget code contained in the jquery.tablesorter.widgets.js file
 		// use the zebra stripe widget if you plan on hiding any rows (filter widget)
-		widgets : [ "uitheme", "filter", "zebra" ],
+		widgets : [ "uitheme", "filter", "zebra", "saveSort" ],
 
 		widgetOptions : {
 		  // using the default zebra striping class name, so it actually isn't included in the theme variable above
