c18c5d301d2f8fa3a5e0de4ec2cb7f9d8f23905d
confuser <jamesmortemore@gmail.com>
2013-06-12 02:37:41 +0100

    Remove mute from memory on player quit

--- src/me/confuserr/banmanager/Listeners/MutedBlacklistCheck.java
+++ src/me/confuserr/banmanager/Listeners/MutedBlacklistCheck.java
@@ -5,6 +5,7 @@ import me.confuserr.banmanager.BanManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class MutedBlacklistCheck implements Listener {
 	private BanManager plugin;
@@ -12,23 +13,28 @@ public class MutedBlacklistCheck implements Listener {
 	public MutedBlacklistCheck(BanManager instance) {
 		plugin = instance;
 	}
-	
+
 	@EventHandler
 	public void onCommand(PlayerCommandPreprocessEvent event) {
-		if(!plugin.isPlayerMuted(event.getPlayer().getName()))
+		if (!plugin.isPlayerMuted(event.getPlayer().getName()))
 			return;
-		
+
 		// Split the command
 		String[] args = event.getMessage().split(" ");
-		
+
 		// Get rid of the first /
 		String cmd = args[0].replace("/", "");
-		
+
 		// Check to see if its blacklisted
-	    if(plugin.getMutedBlacklist().contains(cmd)) {
-	    	// Cancel it
-	    	event.setCancelled(true);
-	    	event.getPlayer().sendMessage(plugin.getMessage("mutedBlacklistedCommand"));
-	    }
+		if (plugin.getMutedBlacklist().contains(cmd)) {
+			// Cancel it
+			event.setCancelled(true);
+			event.getPlayer().sendMessage(plugin.getMessage("mutedBlacklistedCommand"));
+		}
+	}
+
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event) {
+		plugin.getPlayerMutes().remove(event.getPlayer().getName());
 	}
 }
