8a99f2f83e6331d37ab60743007ddedeb72a86e0
confuser <jamesmortemore@gmail.com>
2013-06-16 14:24:53 +0100

    Removed string switch in bmtools for Java 6 compatibility

--- src/me/confuserr/banmanager/Commands/BmToolsCommand.java
+++ src/me/confuserr/banmanager/Commands/BmToolsCommand.java
@@ -27,63 +27,58 @@ public class BmToolsCommand implements CommandExecutor {
 		if (args.length == 0)
 			return false;
 
-		switch (args[0]) {
-			case "listbans":
-				if (sender.hasPermission("bm.tools.listbans")) {
-					String type = "all";
+		if (args[0].equals("listbans")) {
+			if (sender.hasPermission("bm.tools.listbans")) {
+				String type = "all";
 
-					if (args.length == 2) {
-						type = args[1];
-					}
-
-					if (type.equals("all") || type.equals("players")) {
-						String bannedList = "";
-						for (Entry<String, BanData> banned : plugin.getPlayerBans().entrySet()) {
-							bannedList += banned.getKey() + ", ";
-						}
-
-						sender.sendMessage(ChatColor.UNDERLINE + "Banned Players List");
-						if (bannedList.length() > 0)
-							sender.sendMessage(bannedList.substring(0, bannedList.length() - 2));
-						else
-							sender.sendMessage("None");
-					}
-
-					if (type.equals("all") || type.equals("ips")) {
-						String bannedList = "";
-						for (Entry<String, IPBanData> banned : plugin.getIPBans().entrySet()) {
-							bannedList += banned.getKey() + ", ";
-						}
-
-						sender.sendMessage(ChatColor.UNDERLINE + "Banned IPs List");
-						if (bannedList.length() > 0)
-							sender.sendMessage(bannedList.substring(0, bannedList.length() - 2));
-						else
-							sender.sendMessage("None");
-					}
+				if (args.length == 2) {
+					type = args[1];
 				}
-			break;
 
-			case "update":
-				if (sender.hasPermission("bm.tools.update")) {
-					if (!plugin.checkForUpdates())
-						sender.sendMessage(ChatColor.RED + "[BanManager] Please enable update checking within your config!");
-					if (plugin.isUpdateAvailable()) {
-						plugin.getServer().getScheduler().scheduleAsyncDelayedTask(plugin, new Runnable() {
-							public void run() {
-								new Updater(plugin, "ban-management", plugin.jarFile, Updater.UpdateType.NO_VERSION_CHECK, true);
-
-								sender.sendMessage(ChatColor.GREEN + "[BanManager] Updating, please restart server in a moment to apply the update");
-							}
-						});
-					} else {
-						sender.sendMessage(ChatColor.RED + "No updates available.");
+				if (type.equals("all") || type.equals("players")) {
+					String bannedList = "";
+					for (Entry<String, BanData> banned : plugin.getPlayerBans().entrySet()) {
+						bannedList += banned.getKey() + ", ";
 					}
+
+					sender.sendMessage(ChatColor.UNDERLINE + "Banned Players List");
+					if (bannedList.length() > 0)
+						sender.sendMessage(bannedList.substring(0, bannedList.length() - 2));
+					else
+						sender.sendMessage("None");
 				}
-			break;
+
+				if (type.equals("all") || type.equals("ips")) {
+					String bannedList = "";
+					for (Entry<String, IPBanData> banned : plugin.getIPBans().entrySet()) {
+						bannedList += banned.getKey() + ", ";
+					}
+
+					sender.sendMessage(ChatColor.UNDERLINE + "Banned IPs List");
+					if (bannedList.length() > 0)
+						sender.sendMessage(bannedList.substring(0, bannedList.length() - 2));
+					else
+						sender.sendMessage("None");
+				}
+			}
+		} else if (args[0].equals("update")) {
+			if (sender.hasPermission("bm.tools.update")) {
+				if (!plugin.checkForUpdates())
+					sender.sendMessage(ChatColor.RED + "[BanManager] Please enable update checking within your config!");
+				if (plugin.isUpdateAvailable()) {
+					plugin.getServer().getScheduler().scheduleAsyncDelayedTask(plugin, new Runnable() {
+						public void run() {
+							new Updater(plugin, "ban-management", plugin.jarFile, Updater.UpdateType.NO_VERSION_CHECK, true);
+
+							sender.sendMessage(ChatColor.GREEN + "[BanManager] Updating, please restart server in a moment to apply the update");
+						}
+					});
+				} else {
+					sender.sendMessage(ChatColor.RED + "No updates available.");
+				}
+			}
 		}
 
 		return true;
 	}
-
 }
