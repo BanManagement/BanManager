d77b884c8b4ccdd610d699764b9ecc572242ed17
confuser <jamesmortemore@gmail.com>
2013-11-24 13:03:50 +0000

    Fixed password check

--- banmanagement/admin.php
+++ banmanagement/admin.php
@@ -7,7 +7,7 @@
 	may be available at http://creativecommons.org/licenses/by-nc-sa/2.0/uk/.
 	Additional licence terms at https://raw.github.com/confuser/Ban-Management/master/banmanagement/licence.txt
 */
-if(empty($settings['password']) || $settings['passsword'] == 'password')
+if(empty($settings['password']) || $settings['password'] == 'password')
 	errors('You have not set a password. For your security, it\'s required that you set one.');
 else if(isset($_SESSION['failed_attempts']) && $_SESSION['failed_attempts'] > 4) {
 	die(errors('You have reached the maximum number of attempts. Please try again in 30 minutes.'));
