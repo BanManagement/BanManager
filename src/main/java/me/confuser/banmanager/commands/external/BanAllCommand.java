de3fd5538579b5de013baf7b4b074cffe3d84bb0
confuser <jamesmortemore@gmail.com>
2015-01-26 12:53:17 +0000

    Added /banall command

---
+++ src/main/java/me/confuser/banmanager/commands/external/BanAllCommand.java
@@ -0,0 +1,96 @@
+package me.confuser.banmanager.commands.external;
+
+import me.confuser.banmanager.BanManager;
+import me.confuser.banmanager.data.PlayerData;
+import me.confuser.banmanager.data.external.ExternalPlayerBanData;
+import me.confuser.banmanager.util.UUIDUtils;
+import me.confuser.bukkitutil.Message;
+import me.confuser.bukkitutil.commands.BukkitCommand;
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class BanAllCommand extends BukkitCommand<BanManager> {
+
+  public BanAllCommand() {
+    super("banall");
+  }
+
+  @Override
+  public boolean onCommand(final CommandSender sender, Command command, String commandName, String[] args) {
+    if (args.length < 2) {
+      return false;
+    }
+
+    if (args[0].toLowerCase().equals(sender.getName().toLowerCase())) {
+      sender.sendMessage(Message.getString("sender.error.noSelf"));
+      return true;
+    }
+
+    // Check if UUID vs name
+    final String playerName = args[0];
+    final boolean isUUID = playerName.length() > 16;
+
+    final String reason = StringUtils.join(args, " ", 1, args.length);
+
+    plugin.getServer().getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+
+      @Override
+      public void run() {
+        final PlayerData player;
+
+        if (isUUID) {
+          try {
+            player = plugin.getPlayerStorage().queryForId(UUIDUtils.toBytes(UUID.fromString(playerName)));
+          } catch (SQLException e) {
+            sender.sendMessage(Message.get("sender.error.exception").toString());
+            e.printStackTrace();
+            return;
+          }
+        } else {
+          player = plugin.getPlayerStorage().retrieve(playerName, true);
+        }
+
+        if (player == null) {
+          sender.sendMessage(Message.get("sender.error.notFound").set("player", playerName).toString());
+          return;
+        }
+
+        final PlayerData actor;
+
+        if (sender instanceof Player) {
+          actor = plugin.getPlayerStorage().getOnline((Player) sender);
+        } else {
+          actor = plugin.getPlayerStorage().getConsole();
+        }
+
+        final ExternalPlayerBanData ban = new ExternalPlayerBanData(player, actor, reason);
+        int created;
+
+        try {
+          created = plugin.getExternalPlayerBanStorage().create(ban);
+        } catch (SQLException e) {
+          sender.sendMessage(Message.get("sender.error.exception").toString());
+          e.printStackTrace();
+          return;
+        }
+
+        if (created != 1) {
+          return;
+        }
+
+        Message.get("banall.player.notify")
+               .set("actor", ban.getActorName())
+               .set("reason", ban.getReason())
+               .sendTo(sender);
+      }
+
+    });
+
+    return true;
+  }
+}
