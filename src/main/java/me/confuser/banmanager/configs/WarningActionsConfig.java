4faae398e6a3c4ad06c4a1d44db9f7528a5be314
confuser <jamesmortemore@gmail.com>
2015-03-10 01:06:00 +0000

    Commented out warningAction and timeLimit defaults
    Fixed #122

--- src/main/java/me/confuser/banmanager/configs/WarningActionsConfig.java
+++ src/main/java/me/confuser/banmanager/configs/WarningActionsConfig.java
@@ -1,37 +1,37 @@
 package me.confuser.banmanager.configs;
 
-import java.util.HashMap;
-import java.util.List;
-
 import lombok.Getter;
 import me.confuser.banmanager.BanManager;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
 import org.bukkit.configuration.ConfigurationSection;
 
-public class WarningActionsConfig {
-	private BanManager plugin = BanManager.getPlugin();
-	@Getter
-	private boolean isEnabled = false;
-	private HashMap<Integer, List<String>> actions;
+import java.util.HashMap;
+import java.util.List;
 
-	public WarningActionsConfig(ConfigurationSection config) {
-		isEnabled = config.getBoolean("enabled", false);
-		actions = new HashMap<>();
-		
-		for (String amount : config.getConfigurationSection("actions").getKeys(false)) {
-			if (!StringUtils.isNumeric(amount)) {
-				plugin.getLogger().warning("Invalid warning action, " + amount + " is not numeric");
-				continue;
-			}
-			
-			actions.put(NumberUtils.toInt(amount), config.getStringList("actions." + amount));
-		}
-	}
-	
-	public List<String> getCommand(int amount) {
-		return actions.get(amount);
-	}
+public class WarningActionsConfig {
+
+  private BanManager plugin = BanManager.getPlugin();
+  @Getter
+  private boolean isEnabled = false;
+  private HashMap<Integer, List<String>> actions;
+
+  public WarningActionsConfig(ConfigurationSection config) {
+    isEnabled = config.getBoolean("enabled", false);
+    actions = new HashMap<>();
+
+    for (String amount : config.getConfigurationSection("actions").getKeys(false)) {
+      if (!StringUtils.isNumeric(amount)) {
+        plugin.getLogger().warning("Invalid warning action, " + amount + " is not numeric");
+        continue;
+      }
+
+      actions.put(NumberUtils.toInt(amount), config.getStringList("actions." + amount));
+    }
+  }
+
+  public List<String> getCommand(int amount) {
+    return actions.get(amount);
+  }
 
 }
