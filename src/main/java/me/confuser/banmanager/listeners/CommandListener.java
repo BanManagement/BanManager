dba1b252063e82c683260dcae2c0c970e147921c
confuser <jamesmortemore@gmail.com>
2015-08-31 17:38:37 +0100

    Added support for muted blacklisted command arguments
    Fixed #336

--- src/main/java/me/confuser/banmanager/listeners/CommandListener.java
+++ src/main/java/me/confuser/banmanager/listeners/CommandListener.java
@@ -3,6 +3,7 @@ package me.confuser.banmanager.listeners;
 import me.confuser.banmanager.BanManager;
 import me.confuser.bukkitutil.Message;
 import me.confuser.bukkitutil.listeners.Listeners;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
@@ -15,13 +16,22 @@ public class CommandListener extends Listeners<BanManager> {
     }
 
     // Split the command
-    String[] args = event.getMessage().split(" ");
+    String[] args = event.getMessage().split(" ", 6);
 
     // Get rid of the first /
     String cmd = args[0].replace("/", "").toLowerCase();
 
     if (!plugin.getConfiguration().isBlockedCommand(cmd)) {
-      return;
+      // Check if arguments blocked
+      boolean shouldCancel = false;
+      for (int i = 1; i < args.length; i++) {
+        if (plugin.getConfiguration().isBlockedCommand(cmd + " " + StringUtils.join(args, " ", 1, i + 1))) {
+          shouldCancel = true;
+          break;
+        }
+      }
+
+      if (!shouldCancel) return;
     }
 
     event.setCancelled(true);
