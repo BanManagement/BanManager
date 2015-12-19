04ddefe5b9a5b72581f32b77c4359b4e071c3523
James Mortemore <jamesmortemore@gmail.com>
2015-12-19 09:24:46 +0000

    Added max connections to 30 regardless of config
    Fixed #474

--- src/main/java/me/confuser/banmanager/configs/DatabaseConfig.java
+++ src/main/java/me/confuser/banmanager/configs/DatabaseConfig.java
@@ -1,49 +1,50 @@
 package me.confuser.banmanager.configs;
 
-import java.util.HashMap;
-
-import org.bukkit.configuration.ConfigurationSection;
-
 import com.j256.ormlite.table.DatabaseTableConfig;
 import lombok.Getter;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.HashMap;
 
 public abstract class DatabaseConfig {
 
-      @Getter
-      private final String host;
-      @Getter
-      private final int port;
-      @Getter
-      private final String name;
-      @Getter
-      private final String user;
-      @Getter
-      private final String password;
-      @Getter
-      private final boolean isEnabled;
-      @Getter
-      private final int maxConnections;
-      private HashMap<String, DatabaseTableConfig<?>> tables = new HashMap<>();
+  @Getter
+  private final String host;
+  @Getter
+  private final int port;
+  @Getter
+  private final String name;
+  @Getter
+  private final String user;
+  @Getter
+  private final String password;
+  @Getter
+  private final boolean isEnabled;
+  @Getter
+  private int maxConnections;
+  private HashMap<String, DatabaseTableConfig<?>> tables = new HashMap<>();
 
-      public DatabaseConfig(ConfigurationSection conf) {
-            host = conf.getString("host");
-            port = conf.getInt("port", 3306);
-            name = conf.getString("name");
-            user = conf.getString("user");
-            password = conf.getString("password");
-            isEnabled = conf.getBoolean("enabled");
-            maxConnections = conf.getInt("maxConnections", 10);
-      }
+  public DatabaseConfig(ConfigurationSection conf) {
+    host = conf.getString("host");
+    port = conf.getInt("port", 3306);
+    name = conf.getString("name");
+    user = conf.getString("user");
+    password = conf.getString("password");
+    isEnabled = conf.getBoolean("enabled");
+    maxConnections = conf.getInt("maxConnections", 10);
 
-      public String getJDBCUrl() {
-            return "jdbc:mysql://" + host + ":" + port + "/" + name + "?autoReconnect=true&failOverReadOnly=false&maxReconnects=10&useUnicode=true&characterEncoding=utf-8";
-      }
+    if (maxConnections > 30) maxConnections = 30;
+  }
 
-      public DatabaseTableConfig<?> getTable(String table) {
-            return tables.get(table);
-      }
+  public String getJDBCUrl() {
+    return "jdbc:mysql://" + host + ":" + port + "/" + name + "?autoReconnect=true&failOverReadOnly=false&maxReconnects=10&useUnicode=true&characterEncoding=utf-8";
+  }
 
-      public void addTable(String key, DatabaseTableConfig<?> config) {
-            tables.put(key, config);
-      }
+  public DatabaseTableConfig<?> getTable(String table) {
+    return tables.get(table);
+  }
+
+  public void addTable(String key, DatabaseTableConfig<?> config) {
+    tables.put(key, config);
+  }
 }
