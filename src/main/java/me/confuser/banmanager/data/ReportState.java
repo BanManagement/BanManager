ed62d308277cf255ffe02db1f5900bc3230fa3a7
James Mortemore <jamesmortemore@gmail.com>
2016-05-28 19:47:18 +0100

    Linting

--- src/main/java/me/confuser/banmanager/data/ReportState.java
+++ src/main/java/me/confuser/banmanager/data/ReportState.java
@@ -3,7 +3,6 @@ package me.confuser.banmanager.data;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 import lombok.Getter;
-import me.confuser.banmanager.storage.mysql.ByteArray;
 
 @DatabaseTable
 public class ReportState {
