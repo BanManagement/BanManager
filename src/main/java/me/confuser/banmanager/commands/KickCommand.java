2d4d9e6956d38156bf7b79ebbe3739adc000fafb
James Mortemore <jamesmortemore@gmail.com>
2016-06-25 11:19:27 +0100

    Fixed command parsing of kick commands
    Fixed #621

--- src/main/java/me/confuser/banmanager/commands/KickCommand.java
+++ src/main/java/me/confuser/banmanager/commands/KickCommand.java
@@ -5,9 +5,9 @@ import me.confuser.banmanager.data.PlayerData;
 import me.confuser.banmanager.data.PlayerKickData;
 import me.confuser.banmanager.util.CommandParser;
 import me.confuser.banmanager.util.CommandUtils;
-import me.confuser.banmanager.util.UUIDUtils;
 import me.confuser.bukkitutil.Message;
 import me.confuser.bukkitutil.commands.BukkitCommand;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -22,9 +22,16 @@ public class KickCommand extends BukkitCommand<BanManager> {
 
   @Override
   public boolean onCommand(final CommandSender sender, Command command, String commandName, String[] args) {
-    CommandParser parser = new CommandParser(args, 1);
-    args = parser.getArgs();
-    final boolean isSilent = parser.isSilent();
+    final boolean isSilent;
+    CommandParser parser = null;
+
+    if (args.length != 1) {
+      parser = new CommandParser(args, 1);
+      args = parser.getArgs();
+      isSilent = parser.isSilent();
+    } else {
+      isSilent = false;
+    }
 
     if (isSilent && !sender.hasPermission(command.getPermission() + ".silent")) {
       sender.sendMessage(Message.getString("sender.error.noPermission"));
@@ -49,17 +56,17 @@ public class KickCommand extends BukkitCommand<BanManager> {
     final Player player = plugin.getServer().getPlayer(playerName);
 
     if (player == null) {
-      Message message = Message.get("sender.error.offline")
-                               .set("[player]", playerName);
+      Message.get("sender.error.offline")
+             .set("player", playerName)
+             .sendTo(sender);
 
-      sender.sendMessage(message.toString());
       return true;
     } else if (!sender.hasPermission("bm.exempt.override.kick") && player.hasPermission("bm.exempt.kick")) {
       Message.get("sender.error.exempt").set("player", player.getName()).sendTo(sender);
       return true;
     }
 
-    final String reason = args.length > 1 ? parser.getReason().getMessage() : "";
+    final String reason = parser != null ? parser.getReason().getMessage() : "";
 
     plugin.getServer().getScheduler().runTaskAsynchronously(plugin, new Runnable() {
 
