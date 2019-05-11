37a8d80f77f59cdf884332f4ec52cdefd5756868
James Mortemore <jamesmortemore@gmail.com>
2019-05-11 17:48:05 +0100

    fix: use newer mysql connector to support mysql8

--- src/main/java/me/confuser/banmanager/storage/mysql/MySQLDatabase.java
+++ src/main/java/me/confuser/banmanager/storage/mysql/MySQLDatabase.java
@@ -4,7 +4,14 @@ import com.j256.ormlite.db.MysqlDatabaseType;
 
 public class MySQLDatabase extends MysqlDatabaseType {
 
-      public MySQLDatabase() {
-            setCreateTableSuffix("ENGINE=InnoDB DEFAULT CHARSET=utf8");
-      }
+  private final static String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+
+  public MySQLDatabase() {
+    setCreateTableSuffix("ENGINE=InnoDB DEFAULT CHARSET=utf8");
+  }
+
+  @Override
+  protected String getDriverClassName() {
+    return DRIVER_CLASS_NAME;
+  }
 }
