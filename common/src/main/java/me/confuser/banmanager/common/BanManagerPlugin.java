ed98016ff03cd09496be786d3ff7a89fc351c86a
James Mortemore <jamesmortemore@gmail.com>
2019-12-08 13:10:53 +0000

    feat: added AdvancedBan importer (#818)
    usage: /bmimport advancedban 127.0.0.1 3306 databaseName username password
    
    fixes #816

--- common/src/main/java/me/confuser/banmanager/common/BanManagerPlugin.java
+++ common/src/main/java/me/confuser/banmanager/common/BanManagerPlugin.java
@@ -346,6 +346,7 @@ public class BanManagerPlugin {
             new ExportCommand(this),
             new FindAltsCommand(this),
             new InfoCommand(this),
+            new ImportCommand(this),
             new KickCommand(this),
             new LoglessKickCommand(this),
             new MuteCommand(this),
