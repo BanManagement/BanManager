fd5dc1bfde62ab2ad85858740db352b1e07227db
James Mortemore <jamesmortemore@gmail.com>
2020-03-03 21:36:29 +0000

    fix(tests): check local enabled is default

--- common/src/test/java/me/confuser/banmanager/common/PluginTest.java
+++ common/src/test/java/me/confuser/banmanager/common/PluginTest.java
@@ -2,13 +2,12 @@ package me.confuser.banmanager.common;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class PluginTest extends BasePluginTest {
   @Test
   public void testConfigs() {
-    assertFalse(plugin.getConfig().getLocalDb().isEnabled());
+    assertTrue(plugin.getConfig().getLocalDb().isEnabled());
     assertEquals("Console", plugin.getConsoleConfig().getName());
   }
 }
