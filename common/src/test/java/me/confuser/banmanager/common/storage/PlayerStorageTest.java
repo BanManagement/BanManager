be7a1e42d71ec05fcee1d6942904c00ad93f926b
Max Stern <34000717+mxs42@users.noreply.github.com>
2020-04-13 18:39:29 +0300

    fix: always put player names into autoCompleteTree (#866)
    fixes autocomplete in multi-server setup

--- common/src/test/java/me/confuser/banmanager/common/storage/PlayerStorageTest.java
+++ common/src/test/java/me/confuser/banmanager/common/storage/PlayerStorageTest.java
@@ -1,10 +1,14 @@
 package me.confuser.banmanager.common.storage;
 
+import com.j256.ormlite.dao.Dao.CreateOrUpdateStatus;
 import me.confuser.banmanager.common.BasePluginDbTest;
 import me.confuser.banmanager.common.data.PlayerData;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.sql.SQLException;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
 
 public class PlayerStorageTest extends BasePluginDbTest {
 
@@ -14,5 +18,25 @@ public class PlayerStorageTest extends BasePluginDbTest {
 
     assertEquals("Console", data.getName());
     assertEquals(plugin.getConsoleConfig().getUuid(), data.getUUID());
+  }
+
+  @Test
+  public void shouldUpsertPlayerData() throws SQLException {
+    PlayerStorage playerStorage = plugin.getPlayerStorage();
+
+    UUID uuid = UUID.randomUUID();
+
+    PlayerData data = new PlayerData(uuid, "Name 1");
+    CreateOrUpdateStatus status = playerStorage.upsert(data);
+    assertTrue(status.isCreated());
+    assertFalse(status.isUpdated());
+    assertNotNull(playerStorage.getAutoCompleteTree().getValueForExactKey("Name 1"));
+
+    data = new PlayerData(uuid, "Name 2");
+    status = playerStorage.upsert(data);
+    assertFalse(status.isCreated());
+    assertTrue(status.isUpdated());
+    assertNull(playerStorage.getAutoCompleteTree().getValueForExactKey("Name 1"));
+    assertNotNull(playerStorage.getAutoCompleteTree().getValueForExactKey("Name 2"));
   }
 }
