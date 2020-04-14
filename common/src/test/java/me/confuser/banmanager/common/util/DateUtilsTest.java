d709669a797635d2efb21899e8162baf00804c2a
Max Stern <34000717+mxs42@users.noreply.github.com>
2020-04-14 14:55:28 +0300

    fix(test): use UTC explicitly for DateUtils tests (#867)
    fixes #857

--- common/src/test/java/me/confuser/banmanager/common/util/DateUtilsTest.java
+++ common/src/test/java/me/confuser/banmanager/common/util/DateUtilsTest.java
@@ -2,6 +2,8 @@ package me.confuser.banmanager.common.util;
 
 import org.junit.Test;
 
+import java.time.ZoneOffset;
+
 import static org.junit.Assert.assertEquals;
 
 public class DateUtilsTest {
@@ -10,8 +12,8 @@ public class DateUtilsTest {
   public void shouldFormatTimestamp() {
     long timestamp = 1583321267L;
 
-    assertEquals("04-03-2020 11:27:47", DateUtils.format("dd-MM-yyyy HH:mm:ss", timestamp));
-    assertEquals("2020-03-04 11:27:47 +0000", DateUtils.format("yyyy-MM-dd HH:mm:ss Z", timestamp));
-    assertEquals("2020-03-04_11-27-47", DateUtils.format("yyyy-MM-dd_HH-mm-ss", timestamp));
+    assertEquals("04-03-2020 11:27:47", DateUtils.format("dd-MM-yyyy HH:mm:ss", timestamp, ZoneOffset.UTC));
+    assertEquals("2020-03-04 11:27:47 +0000", DateUtils.format("yyyy-MM-dd HH:mm:ss Z", timestamp, ZoneOffset.UTC));
+    assertEquals("2020-03-04_11-27-47", DateUtils.format("yyyy-MM-dd_HH-mm-ss", timestamp, ZoneOffset.UTC));
   }
 }
