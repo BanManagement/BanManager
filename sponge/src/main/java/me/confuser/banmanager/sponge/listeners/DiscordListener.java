6c65eff7da5796786be413761fdc6418543298e9
James Mortemore <jamesmortemore@gmail.com>
2021-02-06 14:52:37 +0000

    feat: use MagiBridge webhooks to show actor name/avatar

--- sponge/src/main/java/me/confuser/banmanager/sponge/listeners/DiscordListener.java
+++ sponge/src/main/java/me/confuser/banmanager/sponge/listeners/DiscordListener.java
@@ -3,6 +3,7 @@ package me.confuser.banmanager.sponge.listeners;
 import com.magitechserver.magibridge.config.FormatType;
 import com.magitechserver.magibridge.discord.DiscordMessageBuilder;
 import me.confuser.banmanager.common.BanManagerPlugin;
+import me.confuser.banmanager.common.data.PlayerData;
 import me.confuser.banmanager.common.listeners.CommonDiscordListener;
 import me.confuser.banmanager.sponge.api.events.*;
 import org.spongepowered.api.event.Listener;
@@ -22,7 +23,7 @@ public class DiscordListener {
   public void notifyOnBan(PlayerBannedEvent event) {
     if (event.isSilent()) return;
 
-    send(listener.notifyOnBan(event.getBan()));
+    send(listener.notifyOnBan(event.getBan()), event.getBan().getActor());
   }
 
   @IsCancelled(Tristate.UNDEFINED)
@@ -30,7 +31,7 @@ public class DiscordListener {
   public void notifyOnMute(PlayerMutedEvent event) {
     if (event.isSilent()) return;
 
-    send(listener.notifyOnMute(event.getMute()));
+    send(listener.notifyOnMute(event.getMute()), event.getMute().getActor());
   }
 
   @IsCancelled(Tristate.UNDEFINED)
@@ -38,7 +39,7 @@ public class DiscordListener {
   public void notifyOnWarn(PlayerWarnedEvent event) {
     if (event.isSilent()) return;
 
-    send(listener.notifyOnWarn(event.getWarning()));
+    send(listener.notifyOnWarn(event.getWarning()), event.getWarning().getActor());
   }
 
   @IsCancelled(Tristate.UNDEFINED)
@@ -46,7 +47,7 @@ public class DiscordListener {
   public void notifyOnBan(IpBannedEvent event) {
     if (event.isSilent()) return;
 
-    send(listener.notifyOnBan(event.getBan()));
+    send(listener.notifyOnBan(event.getBan()), event.getBan().getActor());
   }
 
   @IsCancelled(Tristate.UNDEFINED)
@@ -54,33 +55,35 @@ public class DiscordListener {
   public void notifyOnKick(PlayerKickedEvent event) {
     if (event.isSilent()) return;
 
-    send(listener.notifyOnKick(event.getKick()));
+    send(listener.notifyOnKick(event.getKick()), event.getKick().getActor());
   }
 
   @IsCancelled(Tristate.UNDEFINED)
   @Listener(order = Order.POST)
   public void notifyOnUnban(PlayerUnbanEvent event) {
-    send(listener.notifyOnUnban(event.getBan(), event.getActor(), event.getReason()));
+    send(listener.notifyOnUnban(event.getBan(), event.getActor(), event.getReason()), event.getActor());
   }
 
   @IsCancelled(Tristate.UNDEFINED)
   @Listener(order = Order.POST)
   public void notifyOnUnban(IpUnbanEvent event) {
-    send(listener.notifyOnUnban(event.getBan(), event.getActor(), event.getReason()));
+    send(listener.notifyOnUnban(event.getBan(), event.getActor(), event.getReason()), event.getActor());
   }
 
   @IsCancelled(Tristate.UNDEFINED)
   @Listener(order = Order.POST)
   public void notifyOnUnmute(PlayerUnmuteEvent event) {
-    send(listener.notifyOnUnmute(event.getMute(), event.getActor(), event.getReason()));
+    send(listener.notifyOnUnmute(event.getMute(), event.getActor(), event.getReason()), event.getActor());
   }
 
-  private void send(Object[] data) {
-    FormatType formatType = FormatType.of(() -> data[1].toString());
+  private void send(Object[] data, PlayerData actor) {
     DiscordMessageBuilder
         .forChannel((String) data[0])
-        .format(formatType)
-        .useWebhook(false)
+        .placeholder("message", data[1].toString())
+        .placeholder("player", actor.getName())
+        .placeholder("uuid", actor.getUUID().toString())
+        .placeholder("prefix", "")
+        .format(FormatType.SERVER_TO_DISCORD_FORMAT)
         .send();
   }
 }
