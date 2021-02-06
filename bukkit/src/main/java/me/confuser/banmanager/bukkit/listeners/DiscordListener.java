57f5d5d97b11ac08db71eca9ef8c782b23e92178
James Mortemore <jamesmortemore@gmail.com>
2021-02-06 13:20:20 +0000

    feat: use DiscordSRV webhooks to show actor name/avatar

--- bukkit/src/main/java/me/confuser/banmanager/bukkit/listeners/DiscordListener.java
+++ bukkit/src/main/java/me/confuser/banmanager/bukkit/listeners/DiscordListener.java
@@ -2,17 +2,22 @@ package me.confuser.banmanager.bukkit.listeners;
 
 import github.scarsz.discordsrv.DiscordSRV;
 import github.scarsz.discordsrv.util.DiscordUtil;
+import github.scarsz.discordsrv.util.WebhookUtil;
 import me.confuser.banmanager.bukkit.api.events.*;
 import me.confuser.banmanager.common.BanManagerPlugin;
 import me.confuser.banmanager.common.listeners.CommonDiscordListener;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 public class DiscordListener implements Listener {
+  private BanManagerPlugin plugin;
   private CommonDiscordListener listener;
 
   public DiscordListener(BanManagerPlugin plugin) {
+    this.plugin = plugin;
     this.listener = new CommonDiscordListener(plugin);
   }
 
@@ -22,7 +27,7 @@ public class DiscordListener implements Listener {
 
     Object[] data = listener.notifyOnBan(event.getBan());
 
-    DiscordUtil.sendMessage(DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName((String) data[0]), data[1].toString());
+    send(data, Bukkit.getPlayer(event.getBan().getActor().getUUID()));
   }
 
   @EventHandler(priority = EventPriority.MONITOR)
@@ -31,7 +36,7 @@ public class DiscordListener implements Listener {
 
     Object[] data = listener.notifyOnMute(event.getMute());
 
-    DiscordUtil.sendMessage(DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName((String) data[0]), data[1].toString());
+    send(data, Bukkit.getPlayer(event.getMute().getActor().getUUID()));
   }
 
   @EventHandler(priority = EventPriority.MONITOR)
@@ -40,7 +45,7 @@ public class DiscordListener implements Listener {
 
     Object[] data = listener.notifyOnBan(event.getBan());
 
-    DiscordUtil.sendMessage(DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName((String) data[0]), data[1].toString());
+    send(data, Bukkit.getPlayer(event.getBan().getActor().getUUID()));
   }
 
   @EventHandler(priority = EventPriority.MONITOR)
@@ -49,7 +54,7 @@ public class DiscordListener implements Listener {
 
     Object[] data = listener.notifyOnKick(event.getKick());
 
-    DiscordUtil.sendMessage(DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName((String) data[0]), data[1].toString());
+    send(data, Bukkit.getPlayer(event.getKick().getActor().getUUID()));
   }
 
   @EventHandler(priority = EventPriority.MONITOR)
@@ -58,27 +63,35 @@ public class DiscordListener implements Listener {
 
     Object[] data = listener.notifyOnWarn(event.getWarning());
 
-    DiscordUtil.sendMessage(DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName((String) data[0]), data[1].toString());
+    send(data, Bukkit.getPlayer(event.getWarning().getActor().getUUID()));
   }
 
   @EventHandler(priority = EventPriority.MONITOR)
   public void notifyOnUnban(PlayerUnbanEvent event) {
     Object[] data = listener.notifyOnUnban(event.getBan(), event.getActor(), event.getReason());
 
-    DiscordUtil.sendMessage(DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName((String) data[0]), data[1].toString());
+    send(data, Bukkit.getPlayer(event.getActor().getUUID()));
   }
 
   @EventHandler(priority = EventPriority.MONITOR)
   public void notifyOnUnban(IpUnbanEvent event) {
     Object[] data = listener.notifyOnUnban(event.getBan(), event.getActor(), event.getReason());
 
-    DiscordUtil.sendMessage(DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName((String) data[0]), data[1].toString());
+    send(data, Bukkit.getPlayer(event.getBan().getActor().getUUID()));
   }
 
   @EventHandler(priority = EventPriority.MONITOR)
   public void notifyOnUnmute(PlayerUnmuteEvent event) {
     Object[] data = listener.notifyOnUnmute(event.getMute(), event.getActor(), event.getReason());
 
-    DiscordUtil.sendMessage(DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName((String) data[0]), data[1].toString());
+    send(data, Bukkit.getPlayer(event.getActor().getUUID()));
+  }
+
+  private void send(Object[] data, Player actor) {
+    if (actor == null) {
+      DiscordUtil.sendMessage(DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName((String) data[0]), data[1].toString());
+    } else {
+      WebhookUtil.deliverMessage(DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName((String) data[0]), actor, data[1].toString());
+    }
   }
 }
