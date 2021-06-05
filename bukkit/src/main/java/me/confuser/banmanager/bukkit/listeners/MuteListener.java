2be32aae4e6b7e745229e4a44fa6c4dfbd8a15ad
James Mortemore <jamesmortemore@gmail.com>
2021-06-05 11:27:42 +0100

    feat: block muted players using signs and books
    requires the following permissions:
    bm.block.muted.sign
    bm.block.ipmuted.sign
    bm.block.muted.book
    bm.block.ipmuted.book

--- bukkit/src/main/java/me/confuser/banmanager/bukkit/listeners/MuteListener.java
+++ bukkit/src/main/java/me/confuser/banmanager/bukkit/listeners/MuteListener.java
@@ -8,11 +8,17 @@ import me.confuser.banmanager.common.listeners.CommonMuteListener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.PlayerEditBookEvent;
+
+import java.util.UUID;
 
 public class MuteListener implements Listener {
+  private final BanManagerPlugin plugin;
   private final CommonMuteListener listener;
 
   public MuteListener(BanManagerPlugin plugin) {
+    this.plugin = plugin;
     this.listener = new CommonMuteListener(plugin);
   }
 
@@ -24,5 +30,35 @@ public class MuteListener implements Listener {
   @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
   public void notifyOnMute(IpMutedEvent event) {
     listener.notifyOnMute(event.getMute(), event.isSilent());
+  }
+
+  @EventHandler
+  public void blockOnPlayerMute(SignChangeEvent event) {
+    if (plugin.getPlayerMuteStorage().isMuted(event.getPlayer().getUniqueId()) && event.getPlayer().hasPermission("bm.block.muted.sign")) {
+      event.getBlock().breakNaturally();
+      event.setCancelled(true);
+    }
+  }
+
+  @EventHandler
+  public void blockOnIpMute(SignChangeEvent event) {
+    if (plugin.getIpMuteStorage().isMuted(event.getPlayer().getAddress().getAddress()) && event.getPlayer().hasPermission("bm.block.ipmuted.sign")) {
+      event.getBlock().breakNaturally();
+      event.setCancelled(true);
+    }
+  }
+
+  @EventHandler
+  public void blockOnPlayerMute(PlayerEditBookEvent event) {
+    if (plugin.getPlayerMuteStorage().isMuted(event.getPlayer().getUniqueId()) && event.getPlayer().hasPermission("bm.block.muted.book")) {
+      event.setCancelled(true);
+    }
+  }
+
+  @EventHandler
+  public void blockOnIpMute(PlayerEditBookEvent event) {
+    if (plugin.getIpMuteStorage().isMuted(event.getPlayer().getAddress().getAddress()) && event.getPlayer().hasPermission("bm.block.ipmuted.book")) {
+      event.setCancelled(true);
+    }
   }
 }
