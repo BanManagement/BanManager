1c57a912f085f8fbcdd8737e82782de7b4f284a0
James Mortemore <jamesmortemore@gmail.com>
2021-06-05 11:27:42 +0100

    feat: block muted players using signs and books
    requires the following permissions:
    bm.block.muted.sign
    bm.block.ipmuted.sign
    bm.block.muted.book
    bm.block.ipmuted.book

--- sponge/src/main/java/me/confuser/banmanager/sponge/listeners/MuteListener.java
+++ sponge/src/main/java/me/confuser/banmanager/sponge/listeners/MuteListener.java
@@ -5,15 +5,20 @@ import me.confuser.banmanager.common.BanManagerPlugin;
 import me.confuser.banmanager.common.listeners.CommonMuteListener;
 import me.confuser.banmanager.sponge.api.events.IpMutedEvent;
 import me.confuser.banmanager.sponge.api.events.PlayerMutedEvent;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
+import org.spongepowered.api.event.block.tileentity.ChangeSignEvent;
 import org.spongepowered.api.event.filter.IsCancelled;
+import org.spongepowered.api.event.filter.cause.Root;
 import org.spongepowered.api.util.Tristate;
 
 public class MuteListener {
+  private final BanManagerPlugin plugin;
   private final CommonMuteListener listener;
 
   public MuteListener(BanManagerPlugin plugin) {
+    this.plugin = plugin;
     this.listener = new CommonMuteListener(plugin);
   }
 
@@ -28,4 +33,35 @@ public class MuteListener {
   public void notifyOnMute(IpMutedEvent event) {
     listener.notifyOnMute(event.getMute(), event.isSilent());
   }
+
+  @Listener(order = Order.DEFAULT)
+  public void blockOnPlayerMute(ChangeSignEvent event, @Root Player player) {
+    if (plugin.getPlayerMuteStorage().isMuted(player.getUniqueId()) && player.hasPermission("bm.block.muted.sign")) {
+      event.getTargetTile().getLocation().removeBlock();
+      event.setCancelled(true);
+    }
+  }
+
+  @Listener(order = Order.DEFAULT)
+  public void blockOnIpMute(ChangeSignEvent event, @Root Player player) {
+    if (plugin.getIpMuteStorage().isMuted(player.getConnection().getAddress().getAddress()) && player.hasPermission("bm.block.ipmuted.sign")) {
+      event.getTargetTile().getLocation().removeBlock();
+      event.setCancelled(true);
+    }
+  }
+
+  // Book events unsupported by Sponge API
+//  @Listener(order = Order.DEFAULT)
+//  public void blockOnPlayerMute(PlayerEditBookEvent event, @Root Player player) {
+//    if (plugin.getPlayerMuteStorage().isMuted(player.getUniqueId()) && player.hasPermission("bm.block.muted.book")) {
+//      event.setCancelled(true);
+//    }
+//  }
+//
+//  @Listener(order = Order.DEFAULT)
+//  public void blockOnIpMute(PlayerEditBookEvent event, @Root Player player) {
+//    if (plugin.getIpMuteStorage().isMuted(player.getConnection().getAddress().getAddress()) && player.hasPermission("bm.block.ipmuted.book")) {
+//      event.setCancelled(true);
+//    }
+//  }
 }
