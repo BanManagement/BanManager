7d1407b75ccf7af4688d8918ab1c66d296b81fb0
James Mortemore <jamesmortemore@gmail.com>
2022-01-23 11:01:34 +0000

    fix: javadoc deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 logger.lifecycle("""
 *******************************************
- You are building BanManagmer!
+ You are building BanManager!
  If you encounter trouble:
  1) Try running 'build' in a separate Gradle run
  2) Use gradlew and not gradle
@@ -9,5 +9,9 @@ logger.lifecycle("""
  Output files will be in [subproject]/build/libs
 *******************************************
 """)
+
+plugins {
+    id("io.freefair.aggregate-javadoc") version "6.3.0"
+}
 
 applyRootArtifactoryConfig()
