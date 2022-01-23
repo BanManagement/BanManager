2a91ace7b2060a60a3a5a35f8aee1ae8307edf20
James Mortemore <jamesmortemore@gmail.com>
2022-01-23 18:28:44 +0000

    fix: update link

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ logger.lifecycle("""
  If you encounter trouble:
  1) Try running 'build' in a separate Gradle run
  2) Use gradlew and not gradle
- 3) If you still need help, ask on Discord! Further information https://banmanagement.com/
+ 3) If you still need help, ask on Discord! Further information https://banmanagement.com/support
 
  Output files will be in [subproject]/build/libs
 *******************************************
