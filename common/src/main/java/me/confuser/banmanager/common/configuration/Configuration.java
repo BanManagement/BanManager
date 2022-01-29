3b58e3f15409b088a7d6ef378ec6a6549eb0c9ac
James Mortemore <jamesmortemore@gmail.com>
2022-01-29 20:37:16 +0000

    fix: aggregateJavadoc warnings

--- common/src/main/java/me/confuser/banmanager/common/configuration/Configuration.java
+++ common/src/main/java/me/confuser/banmanager/common/configuration/Configuration.java
@@ -8,11 +8,9 @@ import java.util.Map;
 public interface Configuration extends ConfigurationSection {
     /**
      * Sets the default value of the given path as provided.
-     * <p>
      * <p>If no source {@link Configuration} was provided as a default
      * collection, then a new {@link MemoryConfiguration} will be created to
      * hold the new default value.</p>
-     * <p>
      * <p>If value is null, the value will be removed from the default
      * Configuration source.</p>
      *
@@ -24,23 +22,20 @@ public interface Configuration extends ConfigurationSection {
 
     /**
      * Sets the default values of the given paths as provided.
-     * <p>
      * <p>If no source {@link Configuration} was provided as a default
      * collection, then a new {@link MemoryConfiguration} will be created to
      * hold the new default values.</p>
      *
-     * @param defaults A map of Path->Values to add to defaults.
+     * @param defaults A map of Path-&gt;Values to add to defaults.
      * @throws IllegalArgumentException Thrown if defaults is null.
      */
     void addDefaults(Map<String, Object> defaults);
 
     /**
      * Sets the default values of the given paths as provided.
-     * <p>
      * <p>If no source {@link Configuration} was provided as a default
      * collection, then a new {@link MemoryConfiguration} will be created to
      * hold the new default value.</p>
-     * <p>
      * <p>This method will not hold a reference to the specified Configuration,
      * nor will it automatically update if that Configuration ever changes. If
      * you check this, you should set the default source with {@link
@@ -53,11 +48,9 @@ public interface Configuration extends ConfigurationSection {
 
     /**
      * Gets the source {@link Configuration} for this configuration.
-     * <p>
-     * <p>
      * If no configuration source was set, but default values were added, then
      * a {@link MemoryConfiguration} will be returned. If no source was set
-     * and no defaults were set, then this method will return null.</p>
+     * and no defaults were set, then this method will return null.
      *
      * @return Configuration source for default values, or null if none exist.
      */
@@ -65,7 +58,6 @@ public interface Configuration extends ConfigurationSection {
 
     /**
      * Sets the source of all default values for this {@link Configuration}.
-     * <p>
      * <p>
      * If a previous source was set, or previous default values were defined,
      * then they will not be copied to the new source.</p>
@@ -77,7 +69,6 @@ public interface Configuration extends ConfigurationSection {
 
     /**
      * Gets the {@link ConfigurationOptions} for this {@link Configuration}.
-     * <p>
      * <p>All setters through this method are chainable.</p>
      *
      * @return Options for this configuration
--- common/src/main/java/me/confuser/banmanager/common/configuration/serialization/ConfigurationSerializable.java
+++ common/src/main/java/me/confuser/banmanager/common/configuration/serialization/ConfigurationSerializable.java
@@ -8,11 +8,11 @@ import java.util.Map;
  * the methods as defined by this interface:
  * <ul>
  * <li>A static method "deserialize" that accepts a single {@link Map}&lt;
- * {@link String}, {@link Object}> and returns the class.</li>
+ * {@link String}, {@link Object}&gt; and returns the class.</li>
  * <li>A static method "valueOf" that accepts a single {@link Map}&lt;{@link
- * String}, {@link Object}> and returns the class.</li>
+ * String}, {@link Object}&gt; and returns the class.</li>
  * <li>A constructor that accepts a single {@link Map}&lt;{@link String},
- * {@link Object}>.</li>
+ * {@link Object}&gt;.</li>
  * </ul>
  * In addition to implementing this interface, you must register the class
  * with {@link ConfigurationSerialization#registerClass(Class)}.
@@ -24,7 +24,6 @@ public interface ConfigurationSerializable {
 
   /**
    * Creates a Map representation of this class.
-   * <p>
    * <p>This class must provide a method to restore this class, as defined in
    * the {@link ConfigurationSerializable} interface javadoc.
    *
