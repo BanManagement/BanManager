a34661ed76572b09fc753ca9cafbb240eea47a2c
James Mortemore <jamesmortemore@gmail.com>
2022-03-05 17:50:32 +0000

    feat: publish to maven central repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,15 @@ logger.lifecycle("""
 
 plugins {
     `maven-publish`
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
     id("io.freefair.aggregate-javadoc") version "6.3.0"
 }
 
+nexusPublishing {
+    repositories {
+        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
+}
