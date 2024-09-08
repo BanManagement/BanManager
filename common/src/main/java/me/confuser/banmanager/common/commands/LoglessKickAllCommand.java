8c7aac7f115839fbbbc374fc72cd85ff55724383
James Mortemore <jamesmortemore@gmail.com>
2024-09-08 15:01:49 +0200

    feat: added /kickall & /nlkickall commands (#993)

---
+++ common/src/main/java/me/confuser/banmanager/common/commands/LoglessKickAllCommand.java
@@ -0,0 +1,74 @@
+package me.confuser.banmanager.common.commands;
+
+import me.confuser.banmanager.common.BanManagerPlugin;
+import me.confuser.banmanager.common.CommonPlayer;
+import me.confuser.banmanager.common.data.PlayerData;
+import me.confuser.banmanager.common.util.Message;
+
+public class LoglessKickAllCommand extends CommonCommand {
+
+  public LoglessKickAllCommand(BanManagerPlugin plugin) {
+    super(plugin, "nlkickall", true, 0);
+  }
+
+  @Override
+  public boolean onCommand(final CommonSender sender, CommandParser parser) {
+    final boolean isSilent = parser.isSilent();
+
+    if (parser.isInvalidReason()) {
+      Message.get("sender.error.invalidReason")
+              .set("reason", parser.getReason().getMessage())
+              .sendTo(sender);
+      return true;
+    }
+
+    if (isSilent && !sender.hasPermission(getPermission() + ".silent")) {
+      sender.sendMessage(Message.getString("sender.error.noPermission"));
+      return true;
+    }
+
+    final String reason = parser.args.length > 0 ? parser.getReason().getMessage() : "";
+
+    getPlugin().getScheduler().runAsync(() -> {
+      final PlayerData actor = sender.getData();
+
+      if (actor == null) return;
+
+      getPlugin().getScheduler().runSync(() -> {
+        Message message = Message.get(reason.isEmpty() ? "kickall.notify.noReason" : "kickall.notify.reason");
+        message.set("actor", actor.getName()).set("reason", reason);
+
+        for (CommonPlayer player : getPlugin().getServer().getOnlinePlayers()) {
+          if (!sender.hasPermission("bm.exempt.override.kick") && player.hasPermission("bm.exempt.kick")) {
+            continue;
+          }
+
+          Message kickMessage;
+
+          if (reason.isEmpty()) {
+            kickMessage = Message.get("kickall.player.noReason");
+          } else {
+            kickMessage = Message.get("kickall.player.reason").set("reason", reason);
+          }
+
+          kickMessage
+              .set("displayName", player.getDisplayName())
+              .set("player", player.getName())
+              .set("playerId", player.getUniqueId().toString())
+              .set("actor", actor.getName());
+
+          player.kick(kickMessage.toString());
+        }
+
+        if (isSilent || !sender.hasPermission("bm.notify.kick")) {
+          message.sendTo(sender);
+        }
+
+        if (!isSilent) getPlugin().getServer().broadcast(message.toString(), "bm.notify.kick");
+      });
+    });
+
+    return true;
+  }
+
+}
