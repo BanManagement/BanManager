8b7fc785d084ffb79600bea9baa4abf0e394f2b6
kruthers <pandakruthers@gmail.com>
2026-01-30 18:40:02 +0000

    (Chore) Update other build plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.kikugie.stonecutter") version "0.7.11"
+    id("dev.kikugie.stonecutter") version "0.8.3"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
