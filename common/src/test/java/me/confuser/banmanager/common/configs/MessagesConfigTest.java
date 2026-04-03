990421210472ef9a1ca77e01442b7acd153418b3
James Mortemore <jamesmortemore@gmail.com>
2026-04-03 18:27:19 +0100

    fix: pad empty lines in messages with a space for chat rendering
    Consecutive \n\n in messages.yml produced empty lines that Minecraft's
    chat collapses. Insert a space into empty segments so they render as
    visible blank lines.
    
    Closes #1051

--- common/src/test/java/me/confuser/banmanager/common/configs/MessagesConfigTest.java
+++ common/src/test/java/me/confuser/banmanager/common/configs/MessagesConfigTest.java
@@ -1,10 +1,15 @@
 package me.confuser.banmanager.common.configs;
 
 import me.confuser.banmanager.common.BasePluginTest;
+import me.confuser.banmanager.common.TestLogger;
+import me.confuser.banmanager.common.configuration.file.YamlConfiguration;
 import me.confuser.banmanager.common.util.Message;
 import org.junit.Test;
 
+import java.io.StringReader;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class MessagesConfigTest extends BasePluginTest {
 
@@ -14,5 +19,51 @@ public class MessagesConfigTest extends BasePluginTest {
     assertEquals("&6Currently banned for &4abc&6 by def at 8th July which expires in 1d", Message
             .get("info.ban.temporary").set("reason", "abc").set("actor", "def").set("created",
                     "8th July").set("expires", "1d").toString());
+  }
+
+  @Test
+  public void doubleNewlineProducesNonEmptyLine() {
+    String yaml = "messages:\n  test:\n    greeting: 'Hello\\n\\nWorld'";
+    YamlConfiguration config = YamlConfiguration.loadConfiguration(new StringReader(yaml));
+    Message.load(config, new TestLogger());
+
+    String result = Message.get("test.greeting").toString();
+
+    assertEquals("Hello\n \nWorld", result);
+  }
+
+  @Test
+  public void tripleNewlineProducesNonEmptyLines() {
+    String yaml = "messages:\n  test:\n    greeting: 'Hello\\n\\n\\nWorld'";
+    YamlConfiguration config = YamlConfiguration.loadConfiguration(new StringReader(yaml));
+    Message.load(config, new TestLogger());
+
+    String result = Message.get("test.greeting").toString();
+
+    assertEquals("Hello\n \n \nWorld", result);
+  }
+
+  @Test
+  public void singleNewlineIsUnchanged() {
+    String yaml = "messages:\n  test:\n    greeting: 'Hello\\nWorld'";
+    YamlConfiguration config = YamlConfiguration.loadConfiguration(new StringReader(yaml));
+    Message.load(config, new TestLogger());
+
+    String result = Message.get("test.greeting").toString();
+
+    assertEquals("Hello\nWorld", result);
+  }
+
+  @Test
+  public void noConsecutiveEmptyLinesInLoadedMessages() {
+    String yaml = "messages:\n  test:\n    msg: 'Line1\\n\\nLine2\\n\\n\\nLine3'";
+    YamlConfiguration config = YamlConfiguration.loadConfiguration(new StringReader(yaml));
+    Message.load(config, new TestLogger());
+
+    String result = Message.get("test.msg").toString();
+
+    for (String line : result.split("\n", -1)) {
+      assertFalse("Empty lines should contain a space for chat rendering", line.isEmpty());
+    }
   }
 }
